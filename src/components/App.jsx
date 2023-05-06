52ea1423baca340fd90bd5c01b151c09c54e6059
naturesola <100572919+naturesola@users.noreply.github.com>
2023-05-06 02:11:35 +0100

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function newEmoji(emojiTerm) {
+      return (
+        <Entry
+          key={emojiTerm.id}
+          emoji={emojiTerm.emoji}
+          name={emojiTerm.name}
+          meaning={emojiTerm.meaning}
+          />
+
+      );
+    }
+
+
+function App() {
+  return (
+    <div>
+    <h1>
+      <span>emojipedia</span>
+    </h1>
+
+    <dl className="dictionary">
+      {emojipedia.map(newEmoji)}
+
+      </dl>
+    </div>
+  );
+}
+
+export default App;
